0e3af11a2efbd2cdd1dedf1b254a69bf502bb294
kyrotomato <97704480+kyrotomato@users.noreply.github.com>
2022-07-06 13:38:00 -0500

    made login and sign up buttons responsive and fixed alignment

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -4,8 +4,10 @@ import './landing.css'
 function Landing(){
     return(
         <section id="landing">
+            <h5>Welcome!</h5>
+            <h2>Log in or create an account!</h2>
             <div class="container buttonContainer">
-            <button type='submit' className='btn btn-primary'>Log in</button>
+            <button type='submit' className='btn btn-primary btn-left'>Log in</button>
             <button type='submit' className='btn btn-primary'>Sign Up</button>
             </div>
 
