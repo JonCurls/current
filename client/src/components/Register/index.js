80847bfe9d54cd50be8084e1fee7a13955e6964b
Oath Of Isaac <isaacfgnzlz@gmail.com>
2022-07-07 16:23:58 -0500

    ADD_USER referenced in mutations.js, exported to Register/index.js, useState initial form state set in Register/index.js for register form

--- client/src/components/Register/index.js
+++ client/src/components/Register/index.js
@@ -1,13 +1,13 @@
 import React, {useState} from "react";
 import './register.css';
 import {useMutation} from '@apollo/client';
-import { ADD_USER } from '../../utils/mutations';
-import Auth from '../../utils/auth';
+import { ADD_USER } from '../../utils/mutations'; //TODO: Backend needs to add addUser mutation
+import Auth from '../../utils/auth'; 
 
 const Register = () => {
     // set initial form state
     const [userFormData, setUserFormData] = useState({ username: '', email: '', password: '' });
-    
+
     return(
         <form>
             <input name="email" type="email" id="email" placeholder="your email">
