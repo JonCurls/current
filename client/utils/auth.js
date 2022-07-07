2c0f1a088938191f1db69b19bec2928edbc29011
Oath Of Isaac <isaacfgnzlz@gmail.com>
2022-07-07 14:51:01 -0500

    added loggedIn() to check if there is a saved token in webpage and isTokenExpired() to see if the token has expired in the past second

--- client/utils/auth.js
+++ client/utils/auth.js
@@ -8,6 +8,25 @@ class AuthService {
     getProfile() {
         return decode(this.getToken());
     }
+
+    // check if user is logged in
+    loggedIn() {
+        //checks if there is a saved token and it's still valid
+        const token = this.getToken();
+        return !!token && !this.isTokenExpired(token); //handwaiving here
+    }
+
+    //check if token is expired
+    isTokenExpired(token) {
+        try {
+            const decoded = decode(token);
+            if (decoded.exp < Date.now() / 1000) {
+                return true;
+            } else return false;
+        } catch (err) {
+            return false;
+        }
+    }
 }
 
 export default new AuthService();
