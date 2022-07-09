69c30bad988e2596258a8d848a8521fe66ee5dbe
kyrotomato <97704480+kyrotomato@users.noreply.github.com>
2022-07-09 12:46:38 -0500

    added link elements to landing buttons

--- client/src/components/Landing/index.js
+++ client/src/components/Landing/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./landing.css";
+import { Link } from 'react-router-dom';
 
 function Landing({handlePageChange, activeNav}) {
   return (
@@ -8,12 +9,11 @@ function Landing({handlePageChange, activeNav}) {
       <h5>Welcome!</h5>
       <h2>Log in or create an account!</h2>
       <div className="container buttonContainer">
-        <button type="submit"  onClick={() => handlePageChange('login')} className=" {activeNav === 'login' ? 'active' : ''} btn btn-primary" >
-          Log in
-        </button>
-        <button type="submit"  onClick={() => handlePageChange('register')} className=" {activeNav === 'register' ? 'active' : ''} btn btn-primary">
-          Sign Up
-        </button>
+      <Link to="/login"><button type="submit" className="btn btn-primary" >Log In
+        </button></Link>
+        <Link to="/signup"><button type="submit" className="btn btn-primary">
+          Sign up
+        </button> </Link>
       </div>
     </section>
   );
