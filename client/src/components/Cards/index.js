fb5dfc4ee5e4cfac10c8b7fef320ac3272950ffc
MrCurls <JonCurls@gmail.com>
2022-07-11 10:44:24 -0500

    error management

--- client/src/components/Cards/index.js
+++ client/src/components/Cards/index.js
@@ -1,11 +1,12 @@
-import React, { useState, useEffect } from 'react';
-import { Container, Button, Card, CardColumns } from 'react-bootstrap'
+import React, { useState, useEffect } from "react";
+import { Container, Button, Card } from "react-bootstrap";
+// removed CardColumn from bootstrap
 import { useQuery, useMutation } from "@apollo/client";
 import { GET_ME } from "../../utils/queries";
-import Auth from '../../utils/auth.js';
+import Auth from "../../utils/auth.js";
 import { removeCardId } from "../../utils/localStorage";
 import { REMOVE_CARD } from "../../utils/mutations";
-import './cards.css';
+import "./cards.css";
 
 //overall not necesarry
 // const cardSeed = [
@@ -48,130 +49,133 @@ import './cards.css';
 // ]
 
 function Cards() {
+  const { loading, data } = useQuery(GET_ME);
+  console.log(loading, data);
 
-    const { loading, data } = useQuery(GET_ME)
-    console.log(loading, data)
+  const userData = data?.me || [];
 
-    const userData = data?.me || [];
-
-    // use this to determine if `useEffect()` hook needs to run again
-    const userDataLength = Object.keys(userData).length;
-    useEffect(() => {
-        const getUserData = async () => {
-            try {
-                const token = Auth.loggedIn() ? Auth.getToken() : null;
-
-                if (token) {
-                    return;
-                }
-
-                //TODO this definetly needs to be a gql
-                const getMe = (token) => {
-                    return fetch('/api/users/me', {
-                      headers: {
-                        'Content-Type': 'application/json',
-                        authorization: `Bearer ${token}`,
-                      },
-                    });
-                  };
-
-                const response = await getMe(token);
-
-                if (!response.ok) {
-                    throw new Error("Something went wrong!");
-                }
-
-                const user = await response.json();
-                console.log(user);
-                //setUserData(user); < THis just creates an infinite recursive function
-
-            } catch (err) {
-                console.error(err);
-            }
-        };
-        getUserData();
-    },
-        [userDataLength]
-    );
-
-    const [deleteCard, { error }] = useMutation(REMOVE_CARD);
-    console.log(error);
-
-    // create function that accepts the card"s mongo _id value as param and deletes the card from the database
-    const handleDeleteCard = async (cardId) => {
+  // use this to determine if `useEffect()` hook needs to run again
+  const userDataLength = Object.keys(userData).length;
+  useEffect(() => {
+    const getUserData = async () => {
+      try {
         const token = Auth.loggedIn() ? Auth.getToken() : null;
 
-        if (!token) {
-            return false;
+        if (token) {
+          return;
         }
 
-        try {
-            const response = await deleteCard({
-                variables: { cardId: cardId }
-            });
+        //TODO this definetly needs to be a gql
+        const getMe = (token) => {
+          return fetch("/api/users/me", {
+            headers: {
+              "Content-Type": "application/json",
+              authorization: `Bearer ${token}`,
+            },
+          });
+        };
 
-            if (!response) {
-                throw new Error("something went wrong!");
-            }
+        const response = await getMe(token);
 
-            // upon success, remove card"s id from localStorage
-            removeCardId(cardId);
-        } catch (err) {
-            console.error(err);
+        if (!response.ok) {
+          throw new Error("Something went wrong!");
         }
+
+        const user = await response.json();
+        console.log(user);
+        //setUserData(user); < THis just creates an infinite recursive function
+      } catch (err) {
+        console.error(err);
+      }
     };
+    getUserData();
+  }, [userDataLength]);
 
-    // if data isn"t here yet, say so
-    if (!userDataLength) {
-        return <h2>LOADING...</h2>;
+  const [deleteCard, { error }] = useMutation(REMOVE_CARD);
+  console.log(error);
+
+  // create function that accepts the card"s mongo _id value as param and deletes the card from the database
+  const handleDeleteCard = async (cardId) => {
+    const token = Auth.loggedIn() ? Auth.getToken() : null;
+
+    if (!token) {
+      return false;
     }
 
-    return (
-        <>
-        <Container>
-            <section id="cards">
-                <h2>
-                    {userData.savedCards.length
-                        ? `Viewing ${userData.savedCards.length} saved ${userData.savedCards.length === 1 ? "card" : "cards"}:`
-                        : 'You have no saved cards!'}
-                </h2>
-                <CardColumns>
-                    <div className='container cardContainer'>
-                        {/* {id, link, title, description} */}
-                        {userData.savedCards.map((card) => {
-                            return (
-                                <Card key={card.cardId} border="dark">
-                                    {/*ONLY USE IF WE USE IMAGES {card.image ? <Card.Img src={card.image} alt={`The cover for ${card.title}`} variant="top" /> : null} */}
-                                    <Card.Body>
-                                        <Card.Title>{card.title}</Card.Title>
-                                        <p className="small">Link: {card.title}</p>
-                                        <Card.Text>{card.description}</Card.Text>
-                                        <Button className="btn-block btn-danger" onClick={() => handleDeleteCard(card.cardId)}>
-                                            Delete this Card!
-                                        </Button>
-                                    </Card.Body>
-                                </Card>
-                            );
-                        })}
-                    </div>
-                </CardColumns>
-            </section>
-        </Container>
-        </>
-    );
-};
+    try {
+      const response = await deleteCard({
+        variables: { cardId: cardId },
+      });
+
+      if (!response) {
+        throw new Error("something went wrong!");
+      }
+
+      // upon success, remove card"s id from localStorage
+      removeCardId(cardId);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  // if data isn"t here yet, say so
+  if (!userDataLength) {
+    return <h2>LOADING...</h2>;
+  }
+
+  return (
+    <>
+      <Container>
+        <section id="cards">
+          <h2>
+            {userData.savedCards.length
+              ? `Viewing ${userData.savedCards.length} saved ${
+                  userData.savedCards.length === 1 ? "card" : "cards"
+                }:`
+              : "You have no saved cards!"}
+          </h2>
+          <div className="CardColumns">
+            {/* changed CardColumns to a classname */}
+            <div className="container cardContainer">
+              {/* {id, link, title, description} */}
+              {userData.savedCards.map((card) => {
+                return (
+                  <Card key={card.cardId} border="dark">
+                    {/*ONLY USE IF WE USE IMAGES {card.image ? <Card.Img src={card.image} alt={`The cover for ${card.title}`} variant="top" /> : null} */}
+                    <Card.Body>
+                      <Card.Title>{card.title}</Card.Title>
+                      <p className="small">Link: {card.title}</p>
+                      <Card.Text>{card.description}</Card.Text>
+                      <Button
+                        className="btn-block btn-danger"
+                        onClick={() => handleDeleteCard(card.cardId)}
+                      >
+                        Delete this Card!
+                      </Button>
+                    </Card.Body>
+                  </Card>
+                );
+              })}
+            </div>
+          </div>
+          {/* </CardColumns> */}
+        </section>
+      </Container>
+    </>
+  );
+}
 
 export default Cards;
 
 //     <div className='mapContainer'>
-                                //     <h3>{title}</h3>
-                                //     <div className='cardContent'>
+//     <h3>{title}</h3>
+//     <div className='cardContent'>
 
-                                //     <article key={id} className='cardItem'>    
-                                //     <h4 className='cardItem'>{description}</h4>
-                                //     <div className='cardItem cardItem-btn'>
-                                //     <a href={link} className='btn cardItem'>Link</a>
-                                //     </div>
-                                // </article>
-                                // </div>
-                                // </div>
+//     <article key={id} className='cardItem'>
+//     <h4 className='cardItem'>{description}</h4>
+//     <div className='cardItem cardItem-btn'>
+//     <a href={link} className='btn cardItem'>Link</a>
+//     </div>
+// </article>
+// </div>
+// </div>
