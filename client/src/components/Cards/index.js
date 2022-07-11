0b81ee35792624ff9d55de350678d9d69fe59670
MrCurls <JonCurls@gmail.com>
2022-07-11 09:31:29 -0500

    cards

--- client/src/components/Cards/index.js
+++ client/src/components/Cards/index.js
@@ -1,90 +1,95 @@
-import React from 'react';
-import {Container, Col, Form, Button, Card, CardColumns } from 'react-bootstrap'
+import React from "react";
+import {
+  Container,
+  Col,
+  Form,
+  Button,
+  Card,
+  CardColumns,
+} from "react-bootstrap";
 //import {useMutation} from '@apollo/client';
 import { useQuery, useMutation } from "@apollo/client";
-import {GET_ME} from "../../utils/queries";
-import Auth from '../../utils/auth';
-import './cards.css';
+import { GET_ME } from "../../utils/queries";
+import Auth from "../../utils/auth";
+import "./cards.css";
 
 const card = [
-    {
-        id: 1,
-        link: 'google.com',
-        title: 'First Card',
-        description: 'a summary of the card'
-    },
-    {
-        id: 2,
-        link: 'google.com',
-        title: 'First Card',
-        description: 'a summary of the card'
-    },
-    {
-        id: 3,
-        link: 'google.com',
-        title: 'First Card',
-        description: 'a summary of the card'
-    },
-    {
-        id: 4,
-        link: 'google.com',
-        title: 'First Card',
-        description: 'a summary of the card'
-    },
-    {
-        id: 5,
-        link: 'google.com',
-        title: 'First Card',
-        description: 'a summary of the card'
-    },
-    {
-        id: 6,
-        link: 'google.com',
-        title: 'First Card',
-        description: 'a summary of the card'
-    }
-]
+  {
+    id: 1,
+    link: "google.com",
+    title: "First Card",
+    description: "a summary of the card",
+  },
+  {
+    id: 2,
+    link: "google.com",
+    title: "First Card",
+    description: "a summary of the card",
+  },
+  {
+    id: 3,
+    link: "google.com",
+    title: "First Card",
+    description: "a summary of the card",
+  },
+  {
+    id: 4,
+    link: "google.com",
+    title: "First Card",
+    description: "a summary of the card",
+  },
+  {
+    id: 5,
+    link: "google.com",
+    title: "First Card",
+    description: "a summary of the card",
+  },
+  {
+    id: 6,
+    link: "google.com",
+    title: "First Card",
+    description: "a summary of the card",
+  },
+];
 
-function Cards(){
-    
-        const { loading, data } = useQuery(GET_ME)
-        console.log(loading, data)
+function Cards() {
+  const { loading, data } = useQuery(GET_ME);
+  console.log(loading, data);
 
-        const userData = data?.me || [];
+  const userData = data?.me || [];
 
-         // use this to determine if `useEffect()` hook needs to run again
-        const userDataLength = Object.keys(userData).length;
+  // use this to determine if `useEffect()` hook needs to run again
+  const userDataLength = Object.keys(userData).length;
 
-        const [ deleteCard, { error } ] = useMutation(REMOVE_CARD);
+  // const [ deleteCard, { error } ] = useMutation(REMOVE_CARD);
 
-    return(
-        <Container>
-        <section id="cards">
+  return (
+    <Container>
+      <section id="cards">
         <h2>My Cards</h2>
 
-        <div className='container cardContainer'>
-            {
-                card.map(({id, link, title, description}) => {
-                    return(
-                        <div className='mapContainer'>
-                        <h3>{title}</h3>
-                        <div className='cardContent'>
-                        
-                        <article key={id} className='cardItem'>    
-                        <h4 className='cardItem'>{description}</h4>
-                        <div className='cardItem cardItem-btn'>
-                        <a href={link} className='btn cardItem'>Link</a>
-                        </div>
-                    </article>
+        <div className="container cardContainer">
+          {card.map(({ id, link, title, description }) => {
+            return (
+              <div className="mapContainer">
+                <h3>{title}</h3>
+                <div className="cardContent">
+                  <article key={id} className="cardItem">
+                    <h4 className="cardItem">{description}</h4>
+                    <div className="cardItem cardItem-btn">
+                      <a href={link} className="btn cardItem">
+                        Link
+                      </a>
                     </div>
-                    </div>
-                    )
-                })
-            }
+                  </article>
+                </div>
+              </div>
+            );
+          })}
         </div>
-        </section>
-        </Container>
-    )
+      </section>
+    </Container>
+  );
 }
 
 export default Cards;
