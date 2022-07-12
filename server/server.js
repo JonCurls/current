8a783db3bca95e5edeed3af9cbf93b8133ae555a
MrCurls <JonCurls@gmail.com>
2022-07-12 08:51:42 -0500

    heroku update

--- server/server.js
+++ server/server.js
@@ -22,15 +22,14 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-
 // Serve up static assets
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/build")));
+  app.use(express.static(path.join(__dirname, "../client/src")));
 }
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+  res.sendFile(path.join(__dirname, "../client/src/index.html"));
 });
 
 //create new instance of an Apollo server with the GraphQL schema
