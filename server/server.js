330691dd6d6026be8f68198eeb6e11cf0182f0b2
MrCurls <JonCurls@gmail.com>
2022-07-12 09:03:37 -0500

    file path public

--- server/server.js
+++ server/server.js
@@ -25,11 +25,11 @@ app.use(express.json());
 // Serve up static assets
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/public")));
+  app.use(express.static(path.join(__dirname, "/public")));
 }
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/public/index.js"));
+  res.sendFile(path.join(__dirname, "/public"));
 });
 
 //create new instance of an Apollo server with the GraphQL schema
