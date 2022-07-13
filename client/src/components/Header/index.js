1c0bfe0cbac72ce6f98080a6cec528819ef053ff
MrCurls <JonCurls@gmail.com>
2022-07-13 01:04:47 -0500

    logolink

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,24 +1,20 @@
-import React from 'react';
-import './header.css';
-import currentLogo from '../../Assets/Images/current.png';
+import React from "react";
+import "./header.css";
+import currentLogo from "../../Assets/Images/current.png";
 
 function Header() {
-    return (
-        <div className='headerSection' id="header">
-            <div className="container headerContainer">
-                <div className='header'>
-                    <div className='logoImg'>
-                        <img src={currentLogo} alt="" />
-                    </div>
-                </div>
-            </div>
-
-
-
-
-
-
+  return (
+    <div className="headerSection" id="header">
+      <div className="container headerContainer">
+        <div className="header">
+          <div className="logoImg">
+            <a href="/cards">
+              <img src={currentLogo} alt="logo" />
+            </a>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 export default Header;
