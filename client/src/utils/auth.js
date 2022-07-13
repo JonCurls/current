18dc6d46be17051a03bc40d52da4fb4814ac86da
MrCurls <JonCurls@gmail.com>
2022-07-13 01:25:46 -0500

    window assign home

--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -36,7 +36,7 @@ class AuthService {
   login(idToken) {
     //Saves user token to localStorage
     localStorage.setItem("id_token", idToken);
-    window.location.assign("https://desolate-crag-78193.herokuapp.com/cards");
+    window.location.assign("/");
   }
 
   logout() {
