c22a648188b0db5b58a8704558b0f32b0bbb5f75
Oath Of Isaac <isaacfgnzlz@gmail.com>
2022-07-13 15:52:04 -0500

    12 hours of bugfixing

--- server/server.js
+++ server/server.js
@@ -29,7 +29,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
-app.get("/", (req, res) => {
+app.get(["/", "/cards", "/login", "/signup", "/cards", "/create", "/delete"] , (req,res) => {
   res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
